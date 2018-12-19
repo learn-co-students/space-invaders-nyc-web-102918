8146d241fc90e5ed9cbd61b79fdfad4ac38a6f5c
Matt Migliore <mmigliore311@gmail.com>
2018-12-19 13:19:45 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,23 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields) {
+    this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    if (crew.length > 0) {
+      this.docked = false
+    } else {
+      this.docked = true
+    }
+    this.phasersCharge = "uncharged"
+    this.addCrewMembers()
+  }
+
+  addCrewMembers() {
+    this.crew.forEach(crewMember => {
+      crewMember.currentShip = this
+    })
+  }
+}
