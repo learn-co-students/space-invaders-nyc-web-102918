96f33459b499bb1ba2a36399d14704e737bd08ca
Skylar Salerno <skylar.salerno@gmail.com>
2018-12-19 14:43:57 -0500

    Done.

--- src/spaceship.js
+++ src/spaceship.js
@@ -1,1 +1,18 @@
-class Spaceship {}
+class Spaceship {
+constructor(name,crew,phasers,shields){
+  this.name=name
+  this.crew=crew
+  this.phasers=phasers
+  this.shields=shields
+  this.warpDrive= 'disengaged'
+  if(this.crew.length>0){
+    this.crew.forEach((member)=>{member.currentShip=this})
+    this.docked=false
+  }
+  else{this.docked= true}
+  this.phasersCharge='uncharged'
+  this.cloaked=false;
+  }
+
+
+}
